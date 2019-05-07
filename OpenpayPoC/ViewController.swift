0c6edc3b461729d7856e4c6d86eb9f39dbd167a6
Reyes Sanchez Daniel (RBMX/BUD1-IO2) <Daniel.ReyesSanchez@mx.bosch.com>
2019-05-07 15:33:55 -0500

    initial project

---
+++ OpenpayPoC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenpayPoC
+//
+//  Created by Daniel Reyes Sánchez on 5/7/19.
+//  Copyright © 2019 Daniel Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
